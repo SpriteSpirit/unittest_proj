d72490e3d5775b6f743d68c0e8dc794c3dbb7748
angelinakhalueva <sprite.spirit.t.me@gmail.com>
2023-12-11 01:10:58 +0300

    make 100% coverage

--- tests/test_arrs.py
+++ tests/test_arrs.py
@@ -6,8 +6,17 @@ class TestArrs(unittest.TestCase):
 
     def test_get(self):
         self.assertEqual(arrs.get([1, 2, 3], 1, "test"), 2)
+        self.assertEqual(arrs.get([1, 2], 5, "test"), "test")
         self.assertEqual(arrs.get([], 0, "test"), "test")
+        self.assertEqual(arrs.get([1, 3, 5], -2, "test"), "test")
+        self.assertEqual(arrs.get([1, 3, 5], 2, "test"), 5)
 
     def test_my_slice(self):
         self.assertEqual(arrs.my_slice([1, 2, 3, 4], 1, 3), [2, 3])
         self.assertEqual(arrs.my_slice([1, 2, 3], 1), [2, 3])
+        self.assertEqual(arrs.my_slice([], 1, 4), [])
+        self.assertEqual(arrs.my_slice([1, 2], None, 2), [1, 2])
+        self.assertEqual(arrs.my_slice([1, 2, 3], None, None), [1, 2, 3])
+        self.assertEqual(arrs.my_slice([1, 2, 3], -1, 3), [3])
+        self.assertEqual(arrs.my_slice([1], -1, -2), [])
+        self.assertEqual(arrs.my_slice([1], -1, -2), [])
